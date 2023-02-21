e6bbacfe3545211a10e0a2551e7c4fc42860be81
Igor <thelastwaymusic@gmail.com>
2023-02-21 17:03:21 +0200

    Update: - Updating test suites

--- test/fields.test.ts
+++ test/fields.test.ts
@@ -6,35 +6,33 @@ const filed1 = "key2";
 const getFields = (key: string, key2: string) => ({ fields: [key, key2] });
 
 describe("Fields query", () => {
-  it("chain", () => {
-    const query = new SQBuilder().fields(field).fields(filed1).build();
+  it("Chain", () => {
+    const builtQuery = new SQBuilder().fields(field).fields(filed1).build();
 
-    expect(query).toEqual(getFields(field, filed1));
+    expect(builtQuery).toEqual(getFields(field, filed1));
   });
 
-  it("array", () => {
-    const query = new SQBuilder().fields([field, filed1]).build();
+  it("Array", () => {
+    const builtQuery = new SQBuilder().fields([field, filed1]).build();
 
-    expect(query).toEqual(getFields(field, filed1));
+    expect(builtQuery).toEqual(getFields(field, filed1));
   });
 
-  it("same keys", () => {
-    const query = new SQBuilder()
+  it("Same keys", () => {
+    const builtQuery = new SQBuilder()
       .fields([field, filed1, field, filed1])
       .build();
 
-    expect(query).toEqual(getFields(field, filed1));
+    expect(builtQuery).toEqual(getFields(field, filed1));
   });
 
-  it("merging", () => {
+  it("Merging", () => {
     const query1 = new SQBuilder().fields([field]);
-
     const query2 = new SQBuilder().fields([filed1]);
-
     const query3 = new SQBuilder().fields([field]);
 
-    const query = query1.joinFields(query2).joinFields(query3).build();
+    const builtQuery = query1.joinFields(query2).joinFields(query3).build();
 
-    expect(query).toEqual(getFields(field, filed1));
+    expect(builtQuery).toEqual(getFields(field, filed1));
   });
 });
--- test/sort.test.ts
+++ test/sort.test.ts
@@ -6,74 +6,81 @@ const getOneSort = (key: string) => ({ sort: { [key]: "asc" } });
 const getDeepSort = (key: string, subKey: string) => ({
   sort: { [key]: { [subKey]: "asc" } },
 });
-const getMoreSorts = (key1: string, key2: string) => ({
+const getFewSorts = (key1: string, key2: string) => ({
   sort: [{ [key1]: "asc" }, { [key2]: "asc" }],
 });
 
 describe("Sorting query", () => {
-  it("string", () => {
-    const query = new SQBuilder().sort(oneKey).build();
-    expect(query).toEqual(getOneSort(oneKey));
+  it("String", () => {
+    const builtQuery = new SQBuilder().sort(oneKey).build();
+
+    expect(builtQuery).toEqual(getOneSort(oneKey));
   });
 
-  it("object", () => {
-    const query = new SQBuilder().sort({ key: oneKey, type: "asc" }).build();
-    expect(query).toEqual(getOneSort(oneKey));
+  it("Object", () => {
+    const builtQuery = new SQBuilder().sort({ key: oneKey, type: "asc" }).build();
+
+    expect(builtQuery).toEqual(getOneSort(oneKey));
   });
 
-  it("order", () => {
-    const query = new SQBuilder().sort({ key: oneKey, type: "desc" }).build();
-    expect(query).not.toEqual(getOneSort(oneKey));
+  it("Order", () => {
+    const builtQuery = new SQBuilder().sort({ key: oneKey, type: "desc" }).build();
+
+    expect(builtQuery).not.toEqual(getOneSort(oneKey));
   });
 
-  it("same keys", () => {
-    const query = new SQBuilder()
+  it("Same keys", () => {
+    const builtQuery = new SQBuilder()
       .sort([
         { key: oneKey, type: "asc" },
         { key: oneKey, type: "asc" },
       ])
       .sort([oneKey, oneKey, oneKey])
       .build();
-    expect(query).toEqual(getOneSort(oneKey));
+
+    expect(builtQuery).toEqual(getOneSort(oneKey));
   });
 
-  it("strings", () => {
-    const query = new SQBuilder().sort([oneKey, secondKey]).build();
-    expect(query).toEqual(getMoreSorts(oneKey, secondKey));
+  it("String array", () => {
+    const builtQuery = new SQBuilder().sort([oneKey, secondKey]).build();
+
+    expect(builtQuery).toEqual(getFewSorts(oneKey, secondKey));
   });
 
-  it("objects", () => {
-    const query = new SQBuilder()
+  it("Object array", () => {
+    const builtQuery = new SQBuilder()
       .sort([
         { key: oneKey, type: "asc" },
         { key: secondKey, type: "asc" },
       ])
       .build();
-    expect(query).toEqual(getMoreSorts(oneKey, secondKey));
+
+    expect(builtQuery).toEqual(getFewSorts(oneKey, secondKey));
   });
 
-  it("morph", () => {
-    const query = new SQBuilder()
+  it("Chain", () => {
+    const builtQuery = new SQBuilder()
       .sort(oneKey)
       .sort({ key: secondKey, type: "asc" })
       .build();
 
-    expect(query).toEqual(getMoreSorts(oneKey, secondKey));
+    expect(builtQuery).toEqual(getFewSorts(oneKey, secondKey));
   });
 
-  it("deep sort", () => {
-    const query = new SQBuilder()
+  it("Deep by key", () => {
+    const builtQuery = new SQBuilder()
       .sort([{ key: `${oneKey}.${secondKey}`, type: "asc" }])
       .build();
-    expect(query).toEqual(getDeepSort(oneKey, secondKey));
+
+    expect(builtQuery).toEqual(getDeepSort(oneKey, secondKey));
   });
 
-  it("merging", () => {
+  it("Merging", () => {
     const query1 = new SQBuilder().sort([oneKey]);
     const query2 = new SQBuilder().sort([secondKey]);
 
-    const merged = query1.joinSort(query2).build();
+    const builtQuery = query1.joinSort(query2).build();
 
-    expect(merged).toEqual(getMoreSorts(oneKey, secondKey));
+    expect(builtQuery).toEqual(getFewSorts(oneKey, secondKey));
   });
 });
